1b0dcced96930fc8f086aaca4eec58d6f24d3076
b4020623 <b4020623@my.shu.ac.uk>
2017-04-05 15:37:14 +0100

    Added data samples for Librarian

--- librarySystem/grails-app/init/librarysystem/BootStrap.groovy
+++ librarySystem/grails-app/init/librarysystem/BootStrap.groovy
@@ -1,8 +1,11 @@
 package librarysystem
 
+import com.zafzal.Librarian
 class BootStrap {
 
-    def init = { servletContext ->
+def init = { servletContext ->
+	new Librarian(name:'Harrison Slater', email:'hslater@email.com', office:'44', username:'hslater', password:'password', telephone:'01111111111', library:'Shu').save()
+	new Librarian(name:'Max Atkins', email:'matkins@email.com', office:'69', username:'matkins', password:'cats', telephone:'012345678901', library: 'Shu').save()
     }
     def destroy = {
     }
