dae80f37316bd789cf2701d27948eb726bedd7eb
b4020623 <b4020623@my.shu.ac.uk>
2017-04-05 16:23:17 +0100

    Added data to Course

--- librarySystem/grails-app/init/librarysystem/BootStrap.groovy
+++ librarySystem/grails-app/init/librarysystem/BootStrap.groovy
@@ -2,13 +2,19 @@ package librarysystem
 
 import com.zafzal.Librarian
 import com.zafzal.Book
+import com.zafzal.BookReview
+import com.zafzal.Course
 class BootStrap {
 
 def init = { servletContext ->
 	new Librarian(name:'Harrison Slater', email:'hslater@email.com', office:'44', username:'hslater', password:'password', telephone:'01111111111', library:'Shu').save()
 	new Librarian(name:'Max Atkins', email:'matkins@email.com', office:'69', username:'matkins', password:'cats', telephone:'012345678901', library: 'Shu').save()
-	new Book(title:'Harry Potter', subject:'fantasy', author:'J.K Rowling', isbn:'1111111111111', dateBorrowed:new Date('20/10/2016'), returnDate:new Date('01/01/2017'), name:'John J', overdue:true).save()
-	new Book(title:'Lord of the Rings', subject:'fantasy', author:'J.R Tolkien', isbn:'1111111111122', dateBorrowed:new Date('22/01/2017'), returnDate:new Date('05/06/2017'), name:'Jordan Ash', overdue:true).save()
+	new Book(title:'Harry Potter', subject:'fantasy', author:'J.K Rowling', isbn:'1111111111111', dateBorrowed:new Date('20/10/2016'), returnDate:new Date('01/01/2017'), name:'John J', overdue:false).save()
+	new Book(title:'Lord of the Rings', subject:'fantasy', author:'J.R Tolkien', isbn:'1111111111122', dateBorrowed:new Date('22/01/2017'), returnDate:new Date('05/06/2017'), name:'Jordan Ash', overdue:false).save()
+	new BookReview(book:'The Hunger Games', dateCreated:new Date('22/01/2017'), student:'Michael D', review:'Quite an enjoyable book, would recommend').save()
+	new BookReview(book:'Geography of Britain', dateCreated:new Date('23/01/2017'), student:'Reiss W', review:'Very Informative').save()
+	new Course(title:'Computing', code:'Comp', leader:'David Cameron', department:'IT', description:'Earn a degree based in IT', studyMode:true).save()
+	new Course(title:'Biology', code:'Bio', leader:'Kenny S', department:'Science', description:'Earn a degree based in Biology', studyMode:true).save()
     }
     def destroy = {
     }
